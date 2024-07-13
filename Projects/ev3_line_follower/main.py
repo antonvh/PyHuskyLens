179e570fe0f261b61db2797e87b785209c4723ab
Anton Vanhoucke <antonvh@gmail.com>
2024-07-13 08:13:37 +0200

    Cleanup example

--- Projects/ev3_line_follower/main.py
+++ Projects/ev3_line_follower/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env pybricks-micropython
 
-# This program is for a robot with motors on ports b and c, 
+# This program is for a robot with motors on ports b and c,
 # and a huskylens connected to port S1, in serial mode 9600 baud.
 # On the huskylens 'learn' the line with the teach button.
 # Then run this program to follow the learned line.
+
+# Use VSCode with the MINDSTORMS extension to run it
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (
@@ -25,38 +27,28 @@ from pyhuskylens import HuskyLens, ALGORITHM_LINE_TRACKING
 
 # Create your objects here.
 ev3 = EV3Brick()
-
-
-# Write your program here.
-ev3.speaker.beep()
-print("Beep!")
-
 hl = HuskyLens(Port.S1)
-
-hl.set_alg(ALGORITHM_LINE_TRACKING)
-ev3.speaker.beep()
-hl.clear_text()
-ev3.speaker.beep()
-hl.show_text("EV3 connected")
-ev3.speaker.beep()
-
 lm = Motor(Port.B)
 rm = Motor(Port.C)
 db = DriveBase(lm, rm, 56, 150)
 
-# db.straight(50)
+# Initialize.
+ev3.speaker.beep()
+hl.set_alg(ALGORITHM_LINE_TRACKING)
+hl.show_text("EV3 connected")
 
+# Continuously read arrow data and steer accordingly.
 while 1:
-    data = hl.get_arrows(learned=True)
-    # print(data)
-    if len(data) > 0:
-        head_off_center = data[0].x_head - 160
-        tail_off_center = data[0].x_tail - 160
-        steer = (head_off_center * 10 + tail_off_center * 6) / 16
-        print(steer)
-        db.drive(50,steer*.6)
+    arrow_list = hl.get_arrows(learned=True)
+    # Test if the list of arrows contains at least one item.
+    if len(arrow_list) > 0:
+        arrow = arrow_list[0]
+        head_off_center = arrow.x_head - 160
+        tail_off_center = arrow.x_tail - 160
+        # Steer the robot onto the line. The tail is more important than the head.
+        steer = (head_off_center * 3 + tail_off_center * 10) / 13
+        # Tune the speed values and steer multiplier for a stable run.
+        db.drive(70, steer * 0.75)
     else:
+        # No line to be seen. Stop the engines.
         db.stop()
-        
-    
-    
